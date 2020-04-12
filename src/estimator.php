5995a3be4101d80f30f6fa892052105e52ea53aa
Tolu Ayantunde <ayantundetolu@gmail.com>
2020-04-12 21:25:40 +0100

    challenge 1 adding of first two variable done to the object done

--- src/estimator.php
+++ src/estimator.php
@@ -1,6 +1,24 @@
 <?php
 
+
+
 function covid19ImpactEstimator($data)
 {
-  return $data;
-}
+  $Data = json_decode($data);
+
+  function currentlyFactor($bale){
+    return $Data->reportedCases * $bale;
+  } ;
+
+    
+  $Impact_currentlyInfected = currentlyFactor(10);
+  $SevereImpact_currentlyInfected = currentlyFactor(50);
+     
+
+    return json_decode('{ data : $data,
+          impact: {currentlyInfected:$Impact_currentlyInfected,
+            infectionsByRequestedTime:fffff},
+          severeImpact : {currentlyInfected:$SevereImpact_currentlyInfected,
+            infectionsByRequestedTime:nnnn}
+          }');
+};
