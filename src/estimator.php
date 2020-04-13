9bc86d9299c97367a95ba819af2efb4dcc4ef48d
Tolu Ayantunde <ayantundetolu@gmail.com>
2020-04-13 11:59:38 +0100

    Question 3 annswered and the php function added to round off the figure with out adding decimal

--- src/estimator.php
+++ src/estimator.php
@@ -1,74 +1,101 @@
 <?php
 
+// Declare multi-dimensional array
+
 
 function covid19ImpactEstimator($Data)
 {
-    function currentlyFactor($bale){
+    function currentlyFactor( $Data, $bale){
     return $Data['reportedCases'] * $bale;
-  } 
+      }
 
 
-    //Question 1 
-  $Impact_currentlyInfected = currentlyFactor(10);
+    //Question 1
+  $Impact_currentlyInfected = currentlyFactor($Data,10);
 
-  $SevereImpact_currentlyInfected = currentlyFactor(50);
-     
+  $SevereImpact_currentlyInfected = currentlyFactor($Data,50);
+
   function infectionsByRequestedTimeFactor ($dayss , $InfectedMultiplier ){
-    $calFactorial = $dayss % 3;
-    return $InfectedMultiplier * 2^$calFactorial;
+    // echo($dayss);
+    // echo ",,,,,,,,,,,,,,,,,,";
+     $calFactorial = floor($dayss / 3) ;
+    // echo "the factor is";echo($calFactorial);
+    // echo "answer will be :";echo($InfectedMultiplier * (2^$calFactorial));
+    return $InfectedMultiplier * pow(2,$calFactorial);
     }
 
     switch ($Data["periodType"]) {
       case 'days':
-        $Impact_infectionsByRequestedTime =infectionsByRequestedTimeFactor($Data["timeToElapse"], $Impact_currentlyInfected);
+        $Impact_infectionsByRequestedTime =floor(infectionsByRequestedTimeFactor($Data["timeToElapse"], $Impact_currentlyInfected));
         break;
       case 'weeks':
-        $Impact_infectionsByRequestedTime =infectionsByRequestedTimeFactor(($Data["timeToElapse"] * 7), $Impact_currentlyInfected);      
+        $Impact_infectionsByRequestedTime =floor(infectionsByRequestedTimeFactor(($Data["timeToElapse"] * 7), $Impact_currentlyInfected));
         break;
       case 'months':
-        $Impact_infectionsByRequestedTime =infectionsByRequestedTimeFactor(($Data["timeToElapse"] * 7 * 30), $Impact_currentlyInfected);
+        $Impact_infectionsByRequestedTime =floor(infectionsByRequestedTimeFactor(($Data["timeToElapse"] * 7 * 30), $Impact_currentlyInfected));
         break;
-      
+
       default:
-        $Impact_infectionsByRequestedTime =infectionsByRequestedTimeFactor($Data["timeToElapse"], $Impact_currentlyInfected);
+        $Impact_infectionsByRequestedTime =floor(infectionsByRequestedTimeFactor($Data["timeToElapse"], $Impact_currentlyInfected));
         break;
     }
 
-    switch ($Data->periodType) {
+    switch ($Data["periodType"]) {
       case 'days':
-        $SevereImpact_infectionsByRequestedTime =infectionsByRequestedTimeFactor($Data["timeToElapse"], $SevereImpact_currentlyInfected);
+        $SevereImpact_infectionsByRequestedTime =floor(infectionsByRequestedTimeFactor($Data["timeToElapse"], $SevereImpact_currentlyInfected));
         break;
       case 'weeks':
-        $SevereImpact_infectionsByRequestedTime =infectionsByRequestedTimeFactor(($Data["timeToElapse"] * 7), $SevereImpact_currentlyInfected);      
+        $SevereImpact_infectionsByRequestedTime =floor(infectionsByRequestedTimeFactor(($Data["timeToElapse"] * 7), $SevereImpact_currentlyInfected));
         break;
       case 'months':
-        $SevereImpact_infectionsByRequestedTime =infectionsByRequestedTimeFactor(($Data["timeToElapse"] * 7 * 30), $SevereImpact_currentlyInfected);
+        $SevereImpact_infectionsByRequestedTime =floor(infectionsByRequestedTimeFactor(($Data["timeToElapse"] * 7 * 30), $SevereImpact_currentlyInfected));
         break;
-      
+
       default:
-        $SevereImpact_infectionsByRequestedTime =infectionsByRequestedTimeFactor($Data["timeToElapse"], $SevereImpact_currentlyInfected);
+        $SevereImpact_infectionsByRequestedTime =floor(infectionsByRequestedTimeFactor($Data["timeToElapse"], $SevereImpact_currentlyInfected));
         break;
     }
  //Question 2 answered .....
 
-    $Severe_severeCasesByRequestedTime= 0.15 * $SevereImpact_infectionsByRequestedTime;
-    $Impact_severeCasesByRequestedTime = 0.15 * $Impact_infectionsByRequestedTime;
-    $Impact_hospitalBedsByRequestedTime = ($Data["totalHospitalBeds"] * 0.35 * 0.95) - $Impact_severeCasesByRequestedTime;
-    $Severe_hospitalBedsByRequestedTime = ($Data["totalHospitalBeds"] * 0.35 * 0.95) -  $Severe_severeCasesByRequestedTime;
-    
-    //Question 3 
+    $Severe_severeCasesByRequestedTime=floor( 0.15 * $SevereImpact_infectionsByRequestedTime);
+    $Impact_severeCasesByRequestedTime = floor(0.15 * $Impact_infectionsByRequestedTime);
+    $Impact_hospitalBedsByRequestedTime = floor($Data["totalHospitalBeds"] * 0.35 ) +1- $Impact_severeCasesByRequestedTime;
+    $Severe_hospitalBedsByRequestedTime = floor($Data["totalHospitalBeds"] * 0.35 )+1 - $Severe_severeCasesByRequestedTime;
 
-    $Impact_casesForICUByRequestedTime = $Impact_infectionsByRequestedTime *0.05;
-    $Severe_casesForICUByRequestedTime = $SevereImpact_infectionsByRequestedTime * 0.05;
+    //Question 3
 
-    $Severe_casesForVentilatorsByRequestedTime = $Impact_infectionsByRequestedTime *0.02;
-    $Impact_casesForVentilatorsByRequestedTime =$SevereImpact_infectionsByRequestedTime * 0.02;
-    
-    
-    $Jresult =  [ 'data' => $data , 'impact'=> [ 'currentlyInfected' => $Impact_currentlyInfected ,'infectionsByRequestedTime'=>
-      $Impact_infectionsByRequestedTime,'severeCasesByRequestedTime'=> $Impact_severeCasesByRequestedTime,'hospitalBedsByRequestedTime'=>$Impact_hospitalBedsByRequestedTime,'casesForICUByRequestedTime'=> $Impact_casesForICUByRequestedTime,'casesForVentilatorsByRequestedTime'=>$Impact_casesForVentilatorsByRequestedTime ],'severeImpact' =>['currentlyInfected'=>$SevereImpact_currentlyInfected ,
-          'infectionsByRequestedTime'=>$SevereImpact_infectionsByRequestedTime ,'severeCasesByRequestedTime'=> $Severe_severeCasesByRequestedTime,'hospitalBedsByRequestedTime' =>$Severe_hospitalBedsByRequestedTime,'casesForICUByRequestedTime'=>$Severe_casesForICUByRequestedTime,'casesForVentilatorsByRequestedTime'=>$Severe_casesForVentilatorsByRequestedTime]];
+    $Impact_casesForICUByRequestedTime = floor($Impact_infectionsByRequestedTime *0.05);
+    $Severe_casesForICUByRequestedTime = floor($SevereImpact_infectionsByRequestedTime * 0.05);
+
+    $Severe_casesForVentilatorsByRequestedTime = floor($SevereImpact_infectionsByRequestedTime *0.02);
+    $Impact_casesForVentilatorsByRequestedTime =floor($Impact_infectionsByRequestedTime * 0.02);
+
+    $Impact_dollarsInFlight = floor(($Impact_infectionsByRequestedTime* $Data["region"]["avgDailyIncomeInUSD"]* $Data["region"]["avgDailyIncomePopulation"])/30);
+    $Severe_dollarsInFlight = floor(($SevereImpact_infectionsByRequestedTime * $Data["region"]["avgDailyIncomeInUSD"]* $Data["region"]["avgDailyIncomePopulation"])/30);
+
+
+    $Jresult =  [ 'data' => $Data , 'impact'=> [ 'currentlyInfected' => $Impact_currentlyInfected ,'infectionsByRequestedTime'=>
+      $Impact_infectionsByRequestedTime,'severeCasesByRequestedTime'=> $Impact_severeCasesByRequestedTime,'hospitalBedsByRequestedTime'=>$Impact_hospitalBedsByRequestedTime,'casesForICUByRequestedTime'=> $Impact_casesForICUByRequestedTime,'casesForVentilatorsByRequestedTime'=>$Impact_casesForVentilatorsByRequestedTime, 'dollarsInFlight'=>$Impact_dollarsInFlight ],'severeImpact' =>['currentlyInfected'=>$SevereImpact_currentlyInfected ,
+          'infectionsByRequestedTime'=>$SevereImpact_infectionsByRequestedTime ,'severeCasesByRequestedTime'=> $Severe_severeCasesByRequestedTime,'hospitalBedsByRequestedTime' =>$Severe_hospitalBedsByRequestedTime,'casesForICUByRequestedTime'=>$Severe_casesForICUByRequestedTime,'casesForVentilatorsByRequestedTime'=>$Severe_casesForVentilatorsByRequestedTime,'dollarsInFlight' =>$Severe_dollarsInFlight]];
 
     return json_encode($Jresult) ;
 }
+
+$dadawa= [
+'region'=> [
+'name'=> 'Africa',
+'avgAge'=> '19.7',
+'avgDailyIncomeInUSD'=> '1',
+'avgDailyIncomePopulation'=> '0.65'
+],
+'periodType'=> 'days',
+'timeToElapse'=>'34',
+'reportedCases'=> '1698',
+'population' =>'9956818',
+'totalHospitalBeds'=> '117197'
+];
+echo(covid19ImpactEstimator($dadawa));
+
+
+
 ?>
